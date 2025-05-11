b5c2566d02158553d5203d325cd223a2303c8ab1
Bhargav Saketh Paluvai <103360475+paluvaisaketh@users.noreply.github.com>
2025-05-11 18:41:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ const nodemailer = require("nodemailer");
 const userModel = require("./models/userModel"); 
 const bookingModel = require("./models/bookingModel");
 const app = express();
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public")); 
-
-mongoose.connect("mongodb://localhost:27017/HostelCleaning", {
+const mongoURL   = process.env.mongoURL;
+mongoose.connect(mongoURL+"/HostelCleaning", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("MongoDB connected"))
